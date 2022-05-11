d39c146d84a4413e1e89001ce69a1b9950c8c27a
Sonny Uy Ly <72843106+crispylotus@users.noreply.github.com>
2022-05-11 19:39:01 +1000

    Update EncryptDecrypt.c

--- EncryptDecrypt.c
+++ EncryptDecrypt.c
@@ -6,19 +6,22 @@
 void encrypt_file() {
     char ch;
     FILE *f, *temp;
-
+    
+    /* password file needs to be created before encrypting */
     f = fopen(FILENAME, "r");
     if (f == NULL) {
         printf("File does not exist. \n");
     }
-
+    
+    /* creating temp file */
     temp = fopen("encrypted_passwords.txt", "w");
+    
     while(1) {
         ch = fgetc(f);
         if (ch == EOF) {
             break;
-        } else {
-            ch = ch+100;
+        } else {         /* using substitution ciphering to encrypt data */
+            ch = ch+100; /* value shift to the right 100 values */
             fputc(ch, temp);
         }
     }
@@ -26,9 +29,9 @@ void encrypt_file() {
     fclose(f);
     fclose(temp);
 
+    /* encrypting initial file by copying contents of the temp file */
     f = fopen(FILENAME, "w");
     temp = fopen("encrypted_passwords.txt", "r");
-
     while (1) {
         ch = fgetc(temp);
         if (ch == EOF) {
@@ -47,19 +50,21 @@ void decrypt_file() {
     char ch;
     FILE *f, *temp;
 
+    /* reading from encrypted file */
     f = fopen("encrypted_passwords.txt", "r");
     if (f == NULL) {
         printf("File does not exist. \n");
     }
     
+    /* changnig contents of initial file */
     temp = fopen(FILENAME, "w");
-
+    
     while(1) {
         ch = fgetc(f);
         if (ch == EOF) {
             break;
         } else {
-            ch = ch-100;
+            ch = ch-100; /* shifting values back to original characters */
             fputc(ch, temp);
         }
     }
