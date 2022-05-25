374b00d1f4d44825bf354b20e1c40fda3efc19be
Cassius Makda <101709476+Cassius-Makda@users.noreply.github.com>
2022-05-25 17:56:49 +1000

    Add files via upload
    dont make fun of the name damnit

---
+++ ass3.c
@@ -0,0 +1,72 @@
+/******************************************************************************
+ * 48430 Fundamentals of C Programming - Assignment 3
+ * Name: Cassius Makda
+ * Student ID: 24536599
+
+ * Name: Tan Tri Ngo
+ * Student ID: 
+
+ * Name: Uy Ly
+ * Student ID: 
+
+ * Description:
+ * This is a program that...
+******************************************************************************/
+/* gcc -Werror -Wall -ansi -o ass3.out ass3.c -lm */
+#include <stdio.h>
+#include "compress.h" /* contains our compression functions */
+#inlcude "encrypt.h" /* contains our encryption functions */
+/******************************************************************************
+ * Function prototypes 
+******************************************************************************/
+void printMenu(void);
+
+
+int main()
+{
+    printMenu();
+        int input;
+    do
+    {
+        printMenu();
+        scanf(" %d", &input);
+        while (getchar() != '\n') {}
+        if (input < 1 || input > 6)
+        {
+            printf("You entered an invalid choice. Try again.");
+        }
+        else 
+        { 
+            switch(input) 
+            {
+                case 1:
+                encrypt_file();
+                break;
+
+                case 2:
+                decrypt_file();
+                break;
+
+                case 3:
+
+                break;
+
+                case 4:
+
+                break;
+
+            }
+        }
+    } while(input !=5);
+    return 0;
+}
+
+void printMenu(void){
+    printf("\n\n"
+     "1. Encrypt a file.\n"
+     "2. Decrypt a file (requires an encrypted file).\n"
+     "3. Compress a file.\n"
+     "4. Decompress a file (requires a compressed file).\n"
+     "5. Close the program.\n"
+     "Enter your choice>\n");
+}
