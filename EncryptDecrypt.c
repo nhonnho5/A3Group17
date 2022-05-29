782ef53bf6feff1170ce867489ae85e1fbbb1c6f
Tri Ngo <65775893+nhonnho5@users.noreply.github.com>
2022-05-29 18:33:03 +1000

    Update EncryptDecrypt.c
    final version, please run check for bugs

--- EncryptDecrypt.c
+++ EncryptDecrypt.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define FILENAME "test.txt"
+int key;
 
 /*gcc -Wall -Werror -ansi -o EncryptDecrypt.out EncryptDecrypt.c -lm*/
 /*./EncryptDecrypt.out*/
@@ -16,19 +17,23 @@ void encrypt_file() {
         printf("File does not exist. \n");
     }
     
-    /* creating temp file */
     temp = fopen("encrypted_passwords.txt", "w");
+
+    printf("Enter a 3 digit pin to encrypt file\n");
+    scanf("%3d", &key);
+
+    fputc('~', temp); /*input checker character to see if the file has been encrypted*/
     
     while(1) {
         ch = fgetc(f);
-        if (ch == NULL){
+        if (f == NULL){
             printf("File is empty. \n"); /*check if file is empty*/
             break;
         }
         if (ch == EOF) {
             break;
         } else {         /* using substitution ciphering to encrypt data */
-            ch = ch+100; /* value shift to the right 100 values */
+            ch = ch+key; /* value shift to the right equal to the value of key */
             fputc(ch, temp);
         }
     }
@@ -39,6 +44,7 @@ void encrypt_file() {
     /* encrypting initial file by copying contents of the temp file */
     f = fopen(FILENAME, "w");
     temp = fopen("encrypted_passwords.txt", "r");
+    
     while (1) {
         ch = fgetc(temp);
         if (ch == EOF) {
@@ -62,20 +68,30 @@ void decrypt_file() {
     if (f == NULL) {
         printf("File does not exist. \n");
     }
+
+    ch = fgetc(f);
+    if (ch != '~'){
+        printf("File is not encrypted. Please encrypt the file first. \n");
+        fclose(f);
+        return;
+    }
+
+    printf("Enter a 3 digit pin to decrypt file\n");
+    scanf("%3d", &key);
     
-    /* changnig contents of initial file */
+    /* changing contents of initial file */
     temp = fopen(FILENAME, "w");
     
     while(1) {
         ch = fgetc(f);
-        if (ch == NULL){
+        if (f == NULL){
             printf("File is empty. \n"); /*check if file is empty*/
             break;
         }
         if (ch == EOF) {
             break;
         } else {
-            ch = ch-100; /* shifting values back to original characters */
+            ch = ch-key; /* shifting values back to original characters */
             fputc(ch, temp);
         }
     }
